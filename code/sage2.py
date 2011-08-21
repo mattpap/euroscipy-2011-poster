0e228fa95155c19546bcbb53cb196280710eb8da
Mateusz Paprocki <mattpap@gmail.com>
2011-08-21 21:54:35 +0200

    Updated content

--- code/sage2.py
+++ code/sage2.py
@@ -2,13 +2,12 @@ sage: var('x')
 x
 sage: sin(10).n(digits=10)
 -0.5440211109
-sage: (sin(10)*x).n(digits=10)
-...
-TypeError: ...
 sage: integrate(sin(x), x, 0, 2*pi)
 0
 sage: sin(x) == 1
 sin(x) == 1
-sage: solve(_, x)
+sage: solve(sin(x) == 1, x)
 [x == 1/2*pi]
-
+sage: K.<t> = GF(2)['t']
+sage: factor(t**2 + 1)
+(t + 1)^2
--- code/sympy2.py
+++ code/sympy2.py
@@ -2,13 +2,12 @@
 x
 >>> sin(10).evalf(n=10)
 -0.5440211109
->>> (sin(10)*x).evalf(n=10)
--0.5440211109*x
 >>> integrate(sin(x), (x, 0, 2*pi))
 0
 >>> sin(x) == 1
 False
->>> Eq(sin(x), 1)
-sin(x) = 1
->>> solve(_, x)
+>>> solve(Eq(sin(x), 1), x)
 [pi/2]
+>>> t = Symbol('t')
+>>> factor(t**2 + 1, modulus=2)
+(t + 1)**2
